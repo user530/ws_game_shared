89613ab7bc4b90677359f3490ea94c1ee479fbee
user530 <m.timarsuev@gmail.com>
2024-01-03 21:38:32 +0300

    Lobby listeners added

---
+++ src/interfaces/ws-listeners/game-lobby.ws-listeners.ts
@@ -0,0 +1,16 @@
+import { LobbyEventGuestJoined, LobbyEventGuestLeft, LobbyEventMovedToGame, LobbyEventMovedToHub, ErrorEvent } from '../ws-events';
+import { LobbyCommandStartGame, LobbyCommandLeaveLobby, LobbyCommandKickGuest } from '../ws-messages';
+
+export interface GameLobbyEventsHandler {
+    wsErrorListener(errEvent: ErrorEvent): Promise<void>;
+    wsLobbyGuestJoinedListener(guestJoinedEvent: LobbyEventGuestJoined): Promise<void>;
+    wsLobbyGuestLeftListener(guestLeftEvent: LobbyEventGuestLeft): Promise<void>;
+    wsLobbyGuestToGameListener(movedToGameEvent: LobbyEventMovedToGame): Promise<void>;
+    wsLobbyGuestToHubListener(movedToHubEvent: LobbyEventMovedToHub): Promise<void>;
+};
+
+export interface GameLobbyMessagesHandler {
+    wsLobbyStartGameListener(socket: unknown, startGameMessage: LobbyCommandStartGame): Promise<void>;
+    wsLobbyLeaveLobbyListener(socket: unknown, leaveLobbyMessage: LobbyCommandLeaveLobby): Promise<void>;
+    wsLobbyKickGuestListener(socket: unknown, kickGuestMessage: LobbyCommandKickGuest): Promise<void>;
+};
--- src/interfaces/ws-listeners/index.ts
+++ src/interfaces/ws-listeners/index.ts
@@ -1,6 +1,13 @@
 import { GameInstanceMessagesHandler, GameInstanceEventsHandler } from './game-instance.ws-listener';
+import { GameLobbyMessagesHandler, GameLobbyEventsHandler } from './game-lobby.ws-listeners';
+import { GameHubMessagesHandler, GameHubEventsHandler } from './game-hub.ws-listeners';
+
 
 export {
+    GameHubMessagesHandler,
+    GameHubEventsHandler,
+    GameLobbyMessagesHandler,
+    GameLobbyEventsHandler,
     GameInstanceMessagesHandler,
     GameInstanceEventsHandler,
 }
