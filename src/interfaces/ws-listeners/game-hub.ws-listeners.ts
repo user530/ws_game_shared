b276bfaad9a7ff7e6e796214834a9a00072ad5b1
user530 <m.timarsuev@gmail.com>
2024-01-20 19:45:59 +0300

    Game hub listener updated, added new event

--- src/interfaces/ws-listeners/game-hub.ws-listeners.ts
+++ src/interfaces/ws-listeners/game-hub.ws-listeners.ts
@@ -1,10 +1,11 @@
-import { HubEventGamesUpdated, HubEventMovedToLobby, HubEventQuitHub, ErrorEvent } from '../ws-events';
+import { HubEventGamesUpdated, HubEventMovedToLobby, HubEventMovedToGame, HubEventQuitHub, ErrorEvent } from '../ws-events';
 import { HubCommandHostGame, HubCommandJoinGame, HubCommandLeaveHub } from '../ws-messages';
 
 export interface GameHubEventsHandler {
     wsErrorListener(errEvent: ErrorEvent): Promise<void>;
     wsHubGamesUpdatedListener(gamesUpdatedEvent: HubEventGamesUpdated): Promise<void>;
     wsHubMovedToLobbyListener(movedToLobbyEvent: HubEventMovedToLobby): Promise<void>;
+    wsHubMovedToGameListener(movedToGameEvent: HubEventMovedToGame): Promise<void>;
     wsHubQuitHubListener(quitHubEvent: HubEventQuitHub): Promise<void>;
 };
 
