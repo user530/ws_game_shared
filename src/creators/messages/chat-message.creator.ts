1d1bfb89e6ea99ac8256ba8a37ef85bc5034c981
user530 <m.timarsuev@gmail.com>
2024-02-05 22:20:11 +0300

    Chat message implemented

--- src/creators/messages/chat-message.creator.ts
+++ src/creators/messages/chat-message.creator.ts
@@ -1,6 +1,7 @@
 import { ChatMessage } from '../../interfaces/ws-messages';
 import { MessageType } from '../../types';
+import { MessageData } from '../../interfaces/general';
 
-export const createChatMessage = (messageData: Pick<ChatMessage, 'user' | 'channel' | 'message'>): ChatMessage => {
-    return { version: 1, type: MessageType.ChatMessage, ...messageData };
+export const createChatMessage = (messageData: MessageData): ChatMessage => {
+    return { version: 1, type: MessageType.ChatMessage, messageData };
 };
